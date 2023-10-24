3983cf964aebb6e667731cae29a978826f9c0932
jjthedev <jjthedev94@gmail.com>
2023-10-24 16:49:22 -0600

    added code for questions

--- index.js
+++ index.js
@@ -1,62 +1,89 @@
-// TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-const fs = require('fs');
-const generateHTML = require("./utils/generateHTML");
-// TODO: Create an array of questions for user input
+//Including packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const path = require("path");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+//Creating an array of questions for user input
 const questions = [
-    {
-      type: 'input',
-      name: 'name',
-      message: 'What is your name?',
-    },
-    {
-      type: 'input',
-      name: 'location',
-      message: 'Where are you from?',
-    },
-    {
-      type: 'input',
-      name: 'hobby',
-      message: 'What is your favorite hobby?',
-    },
-    {
-      type: 'input',
-      name: 'food',
-      message: 'What is your favorite food?',
-    },
-    {
-      type: 'input',
-      name: 'github',
-      message: 'Enter your GitHub Username',
-    },
-    {
-      type: 'input',
-      name: 'linkedin',
-      message: 'Enter your LinkedIn URL.',
-    },
-  ];
-// TODO: Create a function to write README file
-function writeToFile(fileName, data){
-    fs.writeFile(fileName,data, (err) =>
-       err ? console.log(err) : console.log('Successfully created index.html!')
-     );
- }
- 
-// TODO: Create a function to initialize app
+  {
+    type: "input",
+    name: "title",
+    message: "Please name your Project.",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please describe the purpose and functionality of this project.",
+  },
+  {
+    type: "input",
+    name: "screenshot",
+    message: "Please provide the relative path to the image you want to use as the screenshot."
+  },
+  {
+    type: "input",
+    name: "link",
+    message: "Please provide a URL where a user can access your deployed application."
+  },
+  {
+    type: "checkbox",
+    name: "license",
+    message: "Please select a license applicable to this project.",
+    choices: ["MIT", "APACHE2.0", "Boost1.0", "MPL2.0", "BSD2", "BSD3", "none"],
+  },
+  {
+    type: "input",
+    name: "require",
+    message: "List any project dependencies here.",
+  },
+  {
+    type: "input",
+    name: "features",
+    message: "List some cool features about this project here.",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message:
+      "State the languages or technologies associated with this project.",
+  },
+  {
+    type: "input",
+    name: "creator",
+    message: "Write your GitHub username.",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Provide a valid email address.",
+  },
+  {
+    type: "input",
+    name: "contributors",
+    message: "Please list any contributors. (Use GitHub usernames)",
+    default: "",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "Provide walkthrough of required tests if applicable.",
+  },
+];
 
-function init(){
-    inquirer
-    .prompt(questions)
-    .then((answers) => {
-      const htmlPageContent = generateHTML(answers);
-  
-      writeToFile("output/index.html", htmlPageContent)
-    });
-  }
-// Function call to initialize app
+// Writing README.md File
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
+
+// Initializing app
+function init() {
+  inquirer.prompt(questions).then((responses) => {
+    console.log("Creating Professional README.md File...");
+    writeToFile("./dist/README.md", generateMarkdown({ ...responses }));
+  });
+}
 init();
-
-
 
 
 
